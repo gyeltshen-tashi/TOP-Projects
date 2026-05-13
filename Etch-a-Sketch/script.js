2bda46f899a3ecdb872c2ee0de2b0d6bda3723fb
Tashi Gyeltshen <gyeltshentashi2001@gmail.com>
2026-05-13 15:38:58 +0600

    Implemented a progressive darkening effect where each interaction darkens the square by 10 %.The goal is to achieve a fully black(or completely colored) square in only ten interactions. Also changed hmtl title

--- Etch-a-Sketch/script.js
+++ Etch-a-Sketch/script.js
@@ -15,12 +15,15 @@ function buildGrid(gridSize = 16) {
 
             const square = document.createElement("div");
             square.classList.add("square");
-
+            let opacity = 10;
             square.addEventListener("mouseenter", () => {
+                if (opacity > 100) return;
                 const r = random();
                 const g = random();
                 const b = random();
-                square.style.backgroundColor = `rgb(${r} ,${g}, ${b})`
+                square.style.opacity = opacity / 100;
+                square.style.backgroundColor = `rgb(${r} ,${g}, ${b})`;
+                opacity += 10;
             });
 
             row.appendChild(square);
@@ -32,7 +35,7 @@ function buildGrid(gridSize = 16) {
 buildGrid();
 
 const squareBtn = document.getElementById("squareBtn");
-squareBtn.addEventListener("click", changeSquares)
+squareBtn.addEventListener("click", changeSquares);
 
 function changeSquares() {
     let newNum = prompt("Enter the number of squares you would like");
@@ -44,11 +47,11 @@ function changeSquares() {
     let parsed = Number(newNum);
     while (parsed < 1 || parsed > 100 || Number.isNaN(parsed)) {
         newNum = prompt("Invalid number. Enter value between 1-100");
-        if (newNum===null) return;
+        if (newNum === null) return;
         parsed = Number(newNum);
     };
     container.replaceChildren();
-    return buildGrid(parsed);
+    buildGrid(parsed);
 }
 
 
